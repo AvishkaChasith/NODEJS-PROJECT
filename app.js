2c7a05461ab32c522f0ac14f92af01ce5c50f278
Avishka Chasith Gunathilaka <chasithgunathilaka@gmail.com>
2024-04-29 23:34:05 +0530

    handle PATCH method

--- app.js
+++ app.js
@@ -18,6 +18,44 @@ app.get("/api/v1/tours", (req, res) => {
     },
   });
 });
+app.get("/api/v1/tours/:id", (req, res) => {
+  const id = req.params.id * 1;
+  if (id > tours.length) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid ID",
+    });
+  }
+  const tour = tours.find((el) => el.id === id);
+  res.status(200).json({
+    status: "success",
+    data: {
+      tour,
+    },
+  });
+});
+
+app.post("/api/v1/tours/:id",(req,res)=>{
+  const id = req.params.id * 1;
+  const tourToUpdate = tours.find(el => el.id === id);
+  if(!tourToUpdate){
+    res.status(404).json({
+      status:"fail",
+      message: "Invalid ID"
+    })
+  }
+  const index = tours.indexOf(tourToUpdate);
+  Object.assign(tourToUpdate,req.body);
+  tours(index) = tourToUpdate;
+  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`,JSON.stringify(tours),(err)=>{
+    res.status(200).json({
+      status: "success",
+      data:{
+        tour:tourToUpdate
+      }
+    })
+  })
+})
 
 app.post("/api/v1/tours", (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
